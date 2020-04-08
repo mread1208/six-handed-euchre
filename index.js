1f7e77f2dcb2650a768f8a168a213fdf2ed1f0a9
Michael Read <Michael.Read@countryfinancial.com>
2020-04-08 16:05:31 -0500

    setting up session storage.

--- index.js
+++ index.js
@@ -1,15 +1,39 @@
 const express = require(`express`);
+const session = require(`express-session`);
+const bodyParser = require(`body-parser`);
+
 const app = express();
 const http = require(`http`).createServer(app);
 const io = require(`socket.io`)(http);
+const port = 4300;
 
-let users = [];
+const users = [];
+
+app.use(session({ secret: `ssshhhhh` }));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(`/static`, express.static(`${__dirname}/static`));
+
+let sess; // global session, NOT recommended
 
 app.get(`/`, function(req, res) {
+    sess = req.session;
+    if (!sess.userName) {
+        console.log("redirect");
+        res.redirect(`/login`);
+    }
     res.sendFile(`${__dirname}/index.html`);
 });
 
-app.use(`/static`, express.static(`${__dirname}/static`));
+app.get(`/login`, function(req, res) {
+    res.sendFile(`${__dirname}/login.html`);
+});
+app.post(`/loginUser`, function(req, res) {
+    sess = req.session;
+    console.log(req.body.username);
+    req.session.userName = req.body.username;
+    res.redirect(`/`);
+});
 
 io.on(`connection`, function(socket) {
     console.log(`a new user connected!`);
@@ -24,6 +48,6 @@ io.on(`connection`, function(socket) {
     });
 });
 
-http.listen(4300, function() {
-    console.log(`listening on *:4300`);
+http.listen(port, function() {
+    console.log(`listening on *:${port}`);
 });
