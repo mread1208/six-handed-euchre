d22aa25bddfeb2f397fa7e7ab188c70f12f19db1
Michael Read <michaelread1208@gmail.com>
2020-04-15 07:41:30 -0500

    Adding form to the game name.

--- apps/six-handed-ng/src/app/games-dashboard/games-dashboard.component.ts
+++ apps/six-handed-ng/src/app/games-dashboard/games-dashboard.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from "@angular/core";
+import { FormBuilder, FormGroup, Validators } from "@angular/forms";
+
 import { GamesService } from "./games.service";
 
 import { Game } from "../models/Game";
@@ -10,8 +12,13 @@ import { Game } from "../models/Game";
 export class GamesDashboardComponent implements OnInit {
     messages: string[] = [];
     rooms: Game[] = [];
+    createRoomForm: FormGroup;
 
-    constructor(private gamesService: GamesService) {}
+    constructor(private gamesService: GamesService, private formBuilder: FormBuilder) {
+        this.createRoomForm = formBuilder.group({
+            gameName: ["", Validators.compose([Validators.required])],
+        });
+    }
 
     ngOnInit(): void {
         this.gamesService.joinGamesDashboard();
@@ -19,12 +26,17 @@ export class GamesDashboardComponent implements OnInit {
             this.rooms = rooms;
         });
     }
-
-    createRoom(roomName, event) {
-        event.preventDefault();
-        this.gamesService.createRoom(roomName);
-    }
     sendMessage(message) {
         this.gamesService.sendMessage(message);
+    }
+
+    createGame() {
+        if (this.createRoomForm.invalid) {
+            return;
+        }
+
+        const gameName = this.createRoomForm.get("gameName").value;
+        this.gamesService.createRoom(gameName);
+        this.createRoomForm.get("gameName").setValue("");
     }
 }
