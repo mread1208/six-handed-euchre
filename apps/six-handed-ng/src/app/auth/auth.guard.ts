76b3cc66ebd7ad9c370e2b70a0223cfe3cee8d91
Michael Read <Michael.Read@countryfinancial.com>
2021-02-19 15:37:27 -0600

    Auth guard

--- apps/six-handed-ng/src/app/auth/auth.guard.ts
+++ apps/six-handed-ng/src/app/auth/auth.guard.ts
@@ -9,7 +9,7 @@ import { AuthService } from "./auth.service";
 export class AuthGuard implements CanActivate {
     constructor(private authService: AuthService, private router: Router) {}
     canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot) {
-        const currentUser = this.authService.currentUserValue;
+        const currentUser = this.authService.getAuthToken();
         if (currentUser) {
             // logged in so return true
             return true;
